93df631dcdebbefcf3b03095270ce146cb3045da
Mykolas Masaitis <9950825@mail.ru>
2022-05-21 18:18:27 +0300

    Добавление пакета «PropertyChanged.Fody.3.4.0»

---
+++ UpExams/ViewModel/BaseViewModel.cs
@@ -0,0 +1,17 @@
+﻿using PropertyChanged;
+using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace UpExams
+{
+    /// <summary>
+    /// A base view model that fires Property Changed events as needed
+    /// </summary>
+    [AddINotifyPropertyChangedInterface]
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+    }
+}
