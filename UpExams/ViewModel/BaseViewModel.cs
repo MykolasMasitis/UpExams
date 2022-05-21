24b125a87172fc0022e4aa243ea3a471998986e4
Mykolas Masaitis <9950825@mail.ru>
2022-05-21 18:22:06 +0300

    Добавлен BaseVieModel класс - базовый класс для всех view model. using PropertyChanged в нем - ссылка на установленный ранее FodyWeaver

--- UpExams/ViewModel/BaseViewModel.cs
+++ UpExams/ViewModel/BaseViewModel.cs
@@ -13,5 +13,14 @@ namespace UpExams
     public class BaseViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        /// <summary>
+        /// Call this to fire a <see cref="PropertyChanged"/> event
+        /// </summary>
+        /// <param name="name"></param>
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
