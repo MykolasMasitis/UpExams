6fdd27d9928d953b0888a143458ae8a750f7c3c9
Mykolas Masaitis <9950825@mail.ru>
2022-05-22 19:55:26 +0300

    Добавлен Frame в главном окне приложения, свойство CurrentPage в WindowViewModel, создано перечисление ApplicationPage - в нем все возможные окна, по мере роста приложения к перечислению добавлять все страницы

---
+++ UpExams/DataModels/ApplicationPage.cs
@@ -0,0 +1,23 @@
+﻿namespace UpExams
+{
+    /// <summary>
+    /// A page of the application
+    /// </summary>
+    public enum ApplicationPage
+    {
+        /// <summary>
+        /// The initial login page
+        /// </summary>
+        MainPage = 0,
+
+        /// <summary>
+        /// The logint page
+        /// </summary>
+        Login = 1,
+
+        /// <summary>
+        /// The register page
+        /// </summary>
+        Register = 2,
+    }
+}
