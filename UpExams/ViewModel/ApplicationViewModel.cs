1892db9a518195f84aea1fe216a08a5f1981f212
Mykolas Masaitis <9950825@mail.ru>
2022-05-26 11:15:22 +0300

    Добавил фунцкцию GoToPage к AppliactionViewModel чтобы не менять страницы командой типа CurrentPage=ApplicationPage.BlankPage

--- UpExams/ViewModel/ApplicationViewModel.cs
+++ UpExams/ViewModel/ApplicationViewModel.cs
@@ -15,6 +15,15 @@ namespace UpExams
         /// The current page of the application
         /// </summary>
         public ApplicationPage CurrentPage { get; set; } = ApplicationPage.MainPage;
+        /// <summary>
+        /// Navigate to the specified page
+        /// </summary>
+        /// <param name="page">The page to go to</param>
+        public void GoTopage(ApplicationPage page)
+        {
+            CurrentPage = page;
+
+        }
 
     }
 }
